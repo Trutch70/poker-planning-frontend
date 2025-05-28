751d15fc0937741a36c54bad93b555bede8f3638
Bartłomiej Banach <b.banach@benefitsystems.pl>
2025-05-28 16:37:37 +0200

    feat: add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+    <nav>
+        <div class="app-banner">
+            <h1 class="app-title">BOBER PLANNING</h1>
+            <img class="logo" src="/bober.svg" alt="Bober Logo" />
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+    .app-banner {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        background-color: #f8f9fa;
+        border-bottom: 1px solid #dee2e6;
+        gap: 10px;
+        justify-content: center;
+    }
+
+    .logo {
+        display: inline-block;
+        width: 100px;
+        transform: scale(1.5) translate(15px, 8px);
+    }
+
+    .app-title {
+        display: inline-block;
+        font-size: 1.5rem;
+        font-weight: bold;
+        color: #343a40;
+        text-transform: uppercase;
+    }
+</style>
